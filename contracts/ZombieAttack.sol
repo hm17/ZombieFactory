76217663d57862865e7695414e3a1083c33bdd73
hazel <hazel.madolid@gmail.com>
2022-03-04 18:48:46 -0600

    added SafeMath

--- contracts/ZombieAttack.sol
+++ contracts/ZombieAttack.sol
@@ -19,13 +19,13 @@ contract ZombieAttack is ZombieHelper {
 
         // We win!
         if(rand <= attackVictoryProbability) {
-            myZombie.winCount++;
-            myZombie.level++;
-            enemyZombie.lossCount++;
+            myZombie.winCount = myZombie.winCount.add(1);
+            myZombie.level = myZombie.level.add(1);
+            enemyZombie.lossCount = enemyZombie.lossCount.add(1);
             feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
         } else {
-            myZombie.lossCount++;
-            enemyZombie.winCount++;
+            myZombie.lossCount = myZombie.lossCount.add(1);
+            enemyZombie.winCount = enemyZombie.winCount.add(1);
             _triggerCooldown(myZombie);
         }
     }
